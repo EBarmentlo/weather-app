e77629ca2a71ef5c7147686253f407f947052f10
EBarmentlo <93001558+EBarmentlo@users.noreply.github.com>
2021-12-09 20:54:21 +0100

    Added project

---
+++ src/script.js
@@ -0,0 +1,77 @@
+//Time
+function realTime() {
+  let currentTime = new Date();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  let day = days[currentTime.getDay()];
+  let hour = currentTime.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minute = currentTime.getMinutes();
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  let date = currentTime.getDate();
+  let month = months[currentTime.getMonth()];
+
+  let time = document.querySelector("#currentDay");
+  time.innerHTML = `${day} ${date} ${month} ${hour}:${minute}`;
+  setInterval(realTime, 1000);
+}
+
+realTime();
+
+//search engine
+
+function showTemperature(response) {
+  document.querySelector("#current-city").innerHTML = response.data.name;
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function search(city) {
+  let apiKey = "42e65d2695e97c180fe2400ea84ef87f";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-text-input").value;
+  search(city);
+}
+
+let searchForm = document.querySelector("#weather-search");
+searchForm.addEventListener("submit", handleSubmit);
